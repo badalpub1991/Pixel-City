bf59ed5cc37b021c04f106cb0f61faf4e3f0919e
Badal-PUB <badalshah@badals-macbook-pro.local>
2018-01-10 18:13:35 +0100

    Initial Commit

---
+++ Pixel-City/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pixel-City
+//
+//  Created by badal shah on 10/01/18.
+//  Copyright © 2018 badal shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
